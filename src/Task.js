07e2eea3c20b7ee8df0f2f1e956ac71ac246b484
repirisyan <repirisyan@icloud.com>
2022-06-23 00:53:41 +0700

    fix delete button

--- src/Task.js
+++ src/Task.js
@@ -31,7 +31,7 @@ class Task extends React.Component {
                                 <div>
                                     <textarea onBlur={this.editItem.bind(this, index)} className="w-full border-transparent">{item.value.text}</textarea>
                                 </div>
-                                <div className="float-right"><button onClick={this.deleteItem.bind(this, item.value.id)} type="button"><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                                <div className="float-right"><button onClick={this.deleteItem.bind(this, index)} type="button"><svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-red-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
                                 </svg></button></div>
                             </div>
